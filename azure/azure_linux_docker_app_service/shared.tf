49ff5aa80c37a2fcbb3019aabddeaff711806f86
Menaga Muthuramalingam <107127252+Menaga12@users.noreply.github.com>
2024-05-14 17:24:07 +0530

    Update shared.tf

--- azure/azure_linux_docker_app_service/shared.tf
+++ azure/azure_linux_docker_app_service/shared.tf
@@ -15,18 +15,6 @@ locals {
   app_insights_name = "${var.name_prefix}app-insights"
 }
 
-resource "azurerm_key_vault" "current" {
-  name                = local.key_vault_name
-  location            = data.azurerm_resource_group.current.location
-  resource_group_name = data.azurerm_resource_group.current.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  soft_delete_enabled        = true
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = false
-
-  sku_name = "standard"
-}
 
 resource "azurerm_container_registry" "current" {
   name                = local.acr_name
